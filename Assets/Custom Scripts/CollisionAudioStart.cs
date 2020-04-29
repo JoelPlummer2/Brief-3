fda7c8d1c97547619db9ece22b75996a5791f0e4
JoelPlummer2 <61271733+JoelPlummer2@users.noreply.github.com>
2020-04-30 02:28:38 +0800

    finished map. needworking on code

---
+++ Assets/Custom Scripts/CollisionAudioStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAudioStart : MonoBehaviour
+{
+
+    public AudioSource enterAudio;
+
+    public void ChangeAenter()
+    {
+        this.gameObject.GetComponent<AudioSource>().Play();
+    }
+}
