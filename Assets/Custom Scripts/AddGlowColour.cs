ae974c3dfc6fd213e8361406299b2d177e32f489
JoelPlummer2 <61271733+JoelPlummer2@users.noreply.github.com>
2020-04-30 18:21:07 +0800

    Game finished( need to fix disco floor)

--- Assets/Custom Scripts/AddGlowColour.cs
+++ Assets/Custom Scripts/AddGlowColour.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 public class AddGlowColour : MonoBehaviour
 {
 
-    public Color enterColor;
-    public Color exitColor;
+
     public Texture enterMaterial;
     public Texture exitMaterial;
     
 
     public void ChangeEnter()
     {
-        this.gameObject.GetComponent<MeshRenderer>().material.color = this.enterColor;
         this.gameObject.GetComponent<MeshRenderer>().material.mainTexture = this.enterMaterial;
     }
 
     public void ChangeExit()
     {
-        this.gameObject.GetComponent<MeshRenderer>().material.color = this.exitColor;
         this.gameObject.GetComponent<MeshRenderer>().material.mainTexture = this.exitMaterial;
     }
 }
